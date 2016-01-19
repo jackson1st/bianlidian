f22d58cdbd6548a192093f98dbe9bbc415c42dd7
jackson1st <Jason@macdeiMac-2.lan>
2016-01-19 20:36:15 +0800

    xx

--- Demo(1)/Demo/NetWork/ContentType.swift
+++ Demo(1)/Demo/NetWork/ContentType.swift
@@ -9,10 +9,7 @@
 import Foundation
 
 public enum ContentType: String{
-<<<<<<< HEAD
-    case
-=======
-    
+
     /**
      *  购物车相关
      */
@@ -150,9 +147,4 @@ public enum ContentType: String{
     //获取用户礼卷
     case StampGet = "/BSMD/getUserPosStamp.do"
     //
-
-    
-    
-    
->>>>>>> origin/master
 }
